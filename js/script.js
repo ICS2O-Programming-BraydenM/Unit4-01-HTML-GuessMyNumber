0931bf9f74042f1be9a505791f895b5e8d35c29e
Brayden MacMillan <99274619+BraydenMacMillan7@users.noreply.github.com>
2022-04-26 15:05:39 +0000

    fixed button

--- js/script.js
+++ js/script.js
@@ -1,27 +1,33 @@
 // Copyright (c) 2022 Brayden MacMillan All rights reserved
 //
-// Created by: Ms Raffin
+// Created by: Brayden MacMillan
 // Created on: April 2022
 // This file contains the JS functions for index.html
 
 "use strict"
 
-/**
- * This function displays a greeting based on the hour of the day
- */
+// This function displays a greeting based on the hour of the day
+ 
 function displayGreeting () {
-  // initialize variables
-	let greeting = ""
-	
-	// get user input
-	let hour = parseInt(document.getElementById('hour').value)
+  // declare constants
+	const MIN = 1;
+  const MAX = 6;
 
+  let correct = Math.floor((Math.random()*MAX)+MIN);
+  
+	// get user input
+	let number = parseInt(document.getElementById('number').value)
 
   // determine the greeting based on the hour of the day in 24-hour format
-	if (hour < 12) {
-		greeting = "Good morning!"
+	if (number == correct) {
+		let greeting = "You are right, that was the number I chose!"
+    document.getElementById('greeting').innerHTML = greeting
 	}
 
-  // display the results
-  document.getElementById('greeting').innerHTML = greeting
+  else {
+    let greeting = "Nope, wrong number. Try again."
+    document.getElementById('greeting').innerHTML = greeting
+  }
+
 }
+
